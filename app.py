13c70bbe7309dd5b75cd68d1dd1e16975131696f
Kiran0365 <96542850+Kiran0365@users.noreply.github.com>
2021-12-24 00:10:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import flask
+from flask import Flask, request , jsonify, render_template
+#import jinja2
+import numpy as np
+import pandas as pd
+import pickle
+
+from flask import Flask, request,jsonify,render_template
+import pickle
+import pandas as pd
+import numpy as np
+get_ipython().system('pip install gunicorn')
+get_ipython().system('pip install -U itsdangerous')
+get_ipython().system('pip install -U MarkupSafe')
+
+
+
+# In[3]:
+
+
+app = Flask(__name__)
+filename = 'Model_GYM.pkl'
+model = pickle.load(open(filename, 'rb'))
+
+
+# In[4]:
+
+
+@app.route('/')
+def man():
+    return render_template('home.html')
+
+
+# In[5]:
+
+
+
+@app.route('/predict', methods=['POST'])
+def home():
+    data1 = request.form['a']
+    data2 = request.form['b']
+    data3 = request.form['c']
+    arr = np.array([[data1, data2, data3]])
+    pred = model.predict(arr)
+    return render_template('after.html',data=pred)
+
+
+# In[6]:
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+    
+
